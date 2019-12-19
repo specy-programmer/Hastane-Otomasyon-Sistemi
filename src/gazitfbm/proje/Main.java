75cb5b7465ae590567ee6d44f84a9278618f5e42
specy-programmer <59061524+specy-programmer@users.noreply.github.com>
2019-12-19 20:51:54 +0300

    Update Main.java

--- src/gazitfbm/proje/Main.java
+++ src/gazitfbm/proje/Main.java
@@ -8,18 +8,6 @@ import gazitfbm.proje.hastaEkranlari.*;
 public final class Main {
     public static void main ( String [] args) {
 
-        /*
-        try{
-            Class.forName("com.mysql.cj.jdbc.Driver");
-            Connection con=DriverManager.getConnection(
-                    "jdbc:mysql://localhost:3306/hastane?useUnicode=true&useLegacyDatetimeCode=false&serverTimezone=Turkey","root","root");
-            Statement stmt=(Statement) con.createStatement();
-            ResultSet rs=((java.sql.Statement) stmt).executeQuery("select * from hastalar");
-            con.close();
-        }catch(Exception e){ System.out.println(e);}
-
-        */
-
         GirisEkrani.girisEkraniGorunumu();
         HastaSecimEkrani.hastaSecimEkraniGorunumu();
         HastaKayitPenceresi.hastaPenceresiGorunumu();
@@ -28,7 +16,6 @@ public final class Main {
         DoktorEkrani.doktorEkraniGorunumu();
         HastaSorgulamaPenceresi.hastaSorgulamaPenceresiGorunumu();
         HastaGuncellePenceresi.hastaGuncellePenceresiGorunumu();
-
 
     }
 }
